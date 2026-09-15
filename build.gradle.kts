04b6b3ab515bb6ae2bd6b487f257ff2014baa612
nwemo8373-prog <nwemo8373@gmail.com>
2026-09-15 23:43:56 +0300

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    // OkHttp للاتصال السريع ببروتوكول التيليجرام
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    // Coroutines للعمل في الخلفية
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+}
